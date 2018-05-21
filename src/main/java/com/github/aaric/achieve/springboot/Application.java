aececcec4798433a5454e5200ab262532584c05a
aaric <vipaaric@gmail.com>
2018-05-21 18:09:45 +0800

    1.3.0-SNAPSHOT i18n

--- src/main/java/com/github/aaric/achieve/springboot/Application.java
+++ src/main/java/com/github/aaric/achieve/springboot/Application.java
@@ -1,4 +1,4 @@
-package com.github.aaric.achieve;
+package com.github.aaric.achieve.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Spring Boot Launcher.
  *
  * @author Aaric, created on 2017-10-23T18:53.
- * @since 1.0-SNAPSHOT
+ * @since 0.0.1-SNAPSHOT
  */
 @SpringBootApplication
 public class Application {
--- src/test/java/com/github/aaric/achieve/springboot/SpringBootFrameworkTest.java
+++ src/test/java/com/github/aaric/achieve/springboot/SpringBootFrameworkTest.java
@@ -1,24 +1,22 @@
-package com.github.aaric.achieve;
+package com.github.aaric.achieve.springboot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.net.UnknownHostException;
-
 /**
  * SpringBootActuatorTest
  *
  * @author Aaric, created on 2017-10-23T19:00.
- * @since 1.0-SNAPSHOT
+ * @since 0.0.0-SNAPSHOT
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootFrameworkTest {
 
     @Test
-    public void testPrint() throws UnknownHostException {
+    public void testPrint() {
         System.out.println("hello");
     }
 }
