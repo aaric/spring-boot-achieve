cf9ffa6b3a87480a858915ac6f49d611b42ab83e
aaric <vipaaric@gmail.com>
2018-10-10 21:53:05 +0800

    0.3.0-SNAPSHOT 数据校验

--- src/main/java/com/github/aaric/achieve/springboot/controller/UserInfoController.java
+++ src/main/java/com/github/aaric/achieve/springboot/controller/UserInfoController.java
@@ -3,10 +3,14 @@ package com.github.aaric.achieve.springboot.controller;
 import com.github.aaric.achieve.springboot.controller.api.UserInfoApi;
 import com.github.aaric.achieve.springboot.entity.UserInfo;
 import com.github.aaric.achieve.springboot.share.JsonMessage;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.github.aaric.achieve.springboot.share.ValidatedMessage;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 用户信息模块控制器
@@ -15,12 +19,32 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 0.2.0-SNAPSHOT
  */
 @RestController
-@RequestMapping("/user/info")
+@RequestMapping("/userInfo")
 public class UserInfoController implements UserInfoApi {
 
     @Override
     @RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
     public JsonMessage<UserInfo> get(@PathVariable("id") Integer id) {
         return new JsonMessage<>(new UserInfo(1, "admin", "admin"));
+    }
+
+    @Override
+    @RequestMapping(value = "/signUp", method = RequestMethod.POST)
+    public JsonMessage<List<ValidatedMessage>> signUp(@RequestBody @Validated UserInfo userInfo, BindingResult bindingResult) {
+        // 打印提交信息
+        System.out.println("## 提交信息");
+        System.out.println(userInfo);
+
+        // 输出数据校验信息
+        if (bindingResult.hasErrors()) {
+            System.out.println("## 校验信息");
+            List<ValidatedMessage> validatedMessageList = new ArrayList<>();
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                System.out.println(fieldError.getField() + ":" + fieldError.getDefaultMessage());
+                validatedMessageList.add(new ValidatedMessage(fieldError.getField(), fieldError.getDefaultMessage()));
+            }
+            return new JsonMessage<>(JsonMessage.DEFAULT_CODE_VALIDATED_ERROR, validatedMessageList);
+        }
+        return new JsonMessage<>();
     }
 }
--- src/main/java/com/github/aaric/achieve/springboot/share/JsonMessage.java
+++ src/main/java/com/github/aaric/achieve/springboot/share/JsonMessage.java
@@ -2,6 +2,7 @@ package com.github.aaric.achieve.springboot.share;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 /**
  * JSON规范对象
@@ -18,9 +19,19 @@ public class JsonMessage<T> {
     public static final String DEFAULT_CODE_SUCCESS = "0000";
 
     /**
+     * 默认数据校验失败状态码
+     */
+    public static final String DEFAULT_CODE_VALIDATED_ERROR = "0001";
+
+    /**
      * 默认成功说明信息
      */
     public static final String DEFAULT_MESSAGE_SUCCESS = "SUCCESS";
+
+    /**
+     * 默认失败说明信息
+     */
+    public static final String DEFAULT_MESSAGE_FAILURE = "FAILURE";
 
     /**
      * 状态码，0000表示正常，其他表示异常
@@ -45,8 +56,25 @@ public class JsonMessage<T> {
         this.message = DEFAULT_MESSAGE_SUCCESS;
     }
 
+    public JsonMessage(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public JsonMessage(String code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
     public JsonMessage(T data) {
         this();
+        this.data = data;
+    }
+
+    public JsonMessage(String code, T data) {
+        this.code = code;
+        this.message = DEFAULT_MESSAGE_FAILURE;
         this.data = data;
     }
 
@@ -72,5 +100,10 @@ public class JsonMessage<T> {
 
     public void setData(T data) {
         this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
     }
 }
